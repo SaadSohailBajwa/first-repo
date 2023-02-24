6f6ba6d49a1b8f0b392a9ecf109c00f1ab83a224
SaadSohailBajwa <125434619+SaadSohailBajwa@users.noreply.github.com>
2023-02-24 11:31:24 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-print("hello world lol")
+stack =[]
 
-hello = "hello"
+sortedList =[]
+
+for i in sortedList:
+  while stackNotEmpty and stack.top()<= i[1]:
+    stack.pop()
+  stack.push(i)
+
+print(stack)  
